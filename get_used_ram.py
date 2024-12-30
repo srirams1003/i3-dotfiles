b49250c9e75021c2fc0d9864e8e050694198021e
Sriram Suresh <sriram.suresh449@gmail.com>
2024-12-30 17:06:32 -0600

    added extra scripts and riced a small bit

---
+++ get_used_ram.py
@@ -0,0 +1,16 @@
+import subprocess
+
+# Run the command and capture the output
+result = subprocess.run(['~/.config/i3/i3blocks-contrib/memory/memory'], 
+                        stdout=subprocess.PIPE, 
+                        text=True, 
+                        shell=True)
+
+# Split the output into lines and process the first line
+output_lines = result.stdout.strip().split('\n')
+if output_lines:
+    # Extract the first number (used RAM)
+    print(output_lines[0].split('/')[0])
+    # used_ram = output_lines[0].split()[0]
+    # print("Used RAM:", used_ram)
+
