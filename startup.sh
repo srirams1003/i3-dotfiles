f5dcfb73dc9528c2c259eb9e937c74600fe323f3
Sriram Suresh <sriram.suresh449@gmail.com>
2025-01-25 18:39:33 -0600

    added sony headphones to auto connect in startup.sh for bluetoothctl

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 sleep 2
 xrandr --output DP-1 --brightness 0.5
-bluetoothctl connect 08:EB:ED:4C:ED:1F
-bluetoothctl connect 4C:FE:2E:28:70:72
+bluetoothctl connect 08:EB:ED:4C:ED:1F # insignia mini sonic
+bluetoothctl connect 4C:FE:2E:28:70:72 # jvc ha-a3t
+bluetoothctl connect E8:9E:13:09:D4:4D # wh-ch520
 
