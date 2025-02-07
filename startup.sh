b917af46945250c1a5343dd20d26e6a52f5d259d
Sriram Suresh <sriram.suresh449@gmail.com>
2025-02-06 21:14:57 -0600

    added xrandr settings to startup for display res

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 sleep 2
 xrandr --output DP-1 --brightness 0.5
+xrandr --output Virtual-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output Virtual-2 --off --output Virtual-3 --off --output Virtual-4 --off --output Virtual-5 --off --output Virtual-6 --off --output Virtual-7 --off --output Virtual-8 --off
 bluetoothctl connect 08:EB:ED:4C:ED:1F # insignia mini sonic
 bluetoothctl connect 4C:FE:2E:28:70:72 # jvc ha-a3t
 bluetoothctl connect E8:9E:13:09:D4:4D # wh-ch520
