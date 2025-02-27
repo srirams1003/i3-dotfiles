acec694558d128ac2b29a688512c190f85c3cce9
Sriram Suresh <sriram.suresh449@gmail.com>
2025-02-26 22:47:45 -0600

    now using goldendict instead of dict for searching

--- dict_screenshot.sh
+++ dict_screenshot.sh
@@ -1,5 +1,11 @@
+# #!/bin/bash
+# maim --format=png --select "/tmp/myverytemporarypicture.png" && 
+# tesseract /tmp/myverytemporarypicture.png /tmp/myverytemporaryoutput && 
+# word=$(cat /tmp/myverytemporaryoutput.txt) &&
+# dict "$word" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+
 #!/bin/bash
 maim --format=png --select "/tmp/myverytemporarypicture.png" && 
 tesseract /tmp/myverytemporarypicture.png /tmp/myverytemporaryoutput && 
 word=$(cat /tmp/myverytemporaryoutput.txt) &&
-dict "$word" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+goldendict "$word"
--- dict_selection.sh
+++ dict_selection.sh
@@ -1,3 +1,7 @@
+# #!/bin/bash
+# selected_text=$(xclip -o -selection primary)
+# dict "$selected_text" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+
 #!/bin/bash
 selected_text=$(xclip -o -selection primary)
-dict "$selected_text" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+goldendict "$selected_text"
