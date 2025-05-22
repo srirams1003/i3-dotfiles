cf20e748e988a799d059e42227863d624c5025bb
Sriram Suresh <sriram.suresh449@gmail.com>
2025-05-21 18:20:04 -0700

    got the toggle_sink.sh script working by ignoring the faulty sink

--- toggle_sink.sh
+++ toggle_sink.sh
@@ -1,46 +1,42 @@
-#! /usr/bin/env bash
-
+#!/usr/bin/env bash
 set -eu
 
-# Get the ID for the current DEFAULT_SINK
-defaultSink=$(pactl info | grep "Default Sink: " | awk '{ print $3 }')
+# The sink we want to exclude (full persistent name)
+TRAP_SINK="alsa_output.pci-0000_02_00.3.Headphones"
 
-# Query the list of all available sinks
+# Get current default sink (full name)
+defaultSink=$(pactl info | grep "Default Sink:" | cut -d' ' -f3)
+echo "Current default: $defaultSink"
+
+# List all sinks, excluding the TRAP_SINK by NAME
 sinks=()
-i=0
-while read line; do
-    index=$(echo $line | awk '{ print $1 }')
-    sinks[${#sinks[@]}]=$index
-
-    # Find the current DEFAULT_SINK
-    if grep -q "$defaultSink" <<< "$line"; then
-        defaultIndex=$index
-        defaultPos=$i
+while read -r id name _; do
+    if [[ "$name" != "$TRAP_SINK" ]]; then
+        sinks+=("$name")
     fi
-
-    i=$(( $i + 1 ))
 done <<< "$(pactl list sinks short)"
 
-# Compute the ID of the new DEFAULT_SINK
-newDefaultPos=$(( ($defaultPos + 1) % ${#sinks[@]} ))
-newDefaultSink=${sinks[$newDefaultPos]}
+echo "Available sinks (excluding trap):"
+printf '%s\n' "${sinks[@]}"
 
-# Update the DEFAULT_SINK
-# pacmd set-default-sink $newDefaultSink
-pactl set-default-sink $newDefaultSink
-
-# these two lines below notify the user of what the current default sink has been changed to
-sinkName=$(pactl list sinks | grep -A10 "Sink #$newDefaultSink" | grep "Description" | cut -d: -f2- | xargs)
-notify-send -u low -h string:x-canonical-private-synchronous:sink "Audio Output: $sinkName"
-
-# Move all current playing streams to the new DEFAULT_SINK
-while read stream; do
-    # Check whether there is a stream playing in the first place
-    if [ -z "$stream" ]; then
+# Find current position in the filtered list
+for i in "${!sinks[@]}"; do
+    if [[ "${sinks[i]}" == "$defaultSink" ]]; then
+        defaultPos=$i
         break
     fi
+done
 
-    streamId=$(echo $stream | awk '{ print $1 }')
-    pactl move-sink-input $streamId @DEFAULT_SINK@
-done <<< "$(pactl list short sink-inputs)"
+# Cycle to next sink (wrap around if needed)
+newDefaultPos=$(( (defaultPos + 1) % ${#sinks[@]} ))
+newDefaultSink="${sinks[$newDefaultPos]}"
 
+# Set new default sink
+pactl set-default-sink "$newDefaultSink"
+
+# Notification with user-friendly name
+sinkName=$(pactl list sinks | grep -A10 "$newDefaultSink" | grep "Description" | cut -d: -f2- | xargs)
+notify-send -u low -h string:x-canonical-private-synchronous:sink "Current Audio Output Device:" "$sinkName"
+
+# Move all active streams to the new sink
+pactl list short sink-inputs | awk '{print $1}' | xargs -r -I{} pactl move-sink-input {} "$newDefaultSink"
