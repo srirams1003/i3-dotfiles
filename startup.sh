e5a09d04e6a383465af03102411e0bc415812516
Sriram Suresh <sriram.suresh449@gmail.com>
2025-05-21 17:25:43 -0700

    made changes after installing i3 on mbp

--- startup.sh
+++ startup.sh
@@ -1,7 +1,11 @@
 sleep 3
-xrandr --output DP-1 --brightness 0.5
+xrandr --output DP-1 --brightness 0.7
+xrandr --output eDP-1 --off
+nmcli device disconnect enxacde48001122 # disabling ethernet on startup
+setxkbmap -option caps:swapescape
+
 # bluetoothctl connect 08:EB:ED:4C:ED:1F # insignia mini sonic
-bluetoothctl connect 08:EB:ED:D2:A8:E5 # insignia mini sonic (new MAC address?!)
-bluetoothctl connect 4C:FE:2E:28:70:72 # jvc ha-a3t
-bluetoothctl connect E8:9E:13:09:D4:4D # wh-ch520
+# bluetoothctl connect 08:EB:ED:D2:A8:E5 # insignia mini sonic (new MAC address?!)
+# bluetoothctl connect 4C:FE:2E:28:70:72 # jvc ha-a3t
+# bluetoothctl connect E8:9E:13:09:D4:4D # wh-ch520
 
