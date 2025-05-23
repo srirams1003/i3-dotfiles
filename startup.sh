c744a0d38e1e512a7372a4fdd2223f5adad20af6
Sriram Suresh <sriram.suresh449@gmail.com>
2025-05-23 07:53:22 -0700

    made changes for mint and lightdm with slick greeter to set external monitor display correctly using xrandr (i hope)

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 sleep 3
-xrandr --output DP-1 --brightness 0.7
 xrandr --output eDP-1 --off
+xrandr --output DP-1 --auto --mode 1920x1080 --brightness 0.7
 nmcli device disconnect enxacde48001122 # disabling ethernet on startup
 setxkbmap -option caps:swapescape
 i3-msg restart
