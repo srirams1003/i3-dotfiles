e3fa5f2a11ee31d392325ebfb2230d4a58e874f8
Sriram Suresh <sriram.suresh449@gmail.com>
2025-06-23 12:37:49 -0700

    made changes for using on i3 dell laptop

--- increase_brightness.sh
+++ increase_brightness.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-output="DP-1"  # Replace this with your monitor name (use `xrandr` to find it)
+# output="DP-1"  # Replace this with your monitor name (use `xrandr` to find it)
+output="HDMI-1"  # Replace this with your monitor name (use `xrandr` to find it)
 step=0.1       # Step size for brightness adjustment
 
 current=$(xrandr --verbose | grep -m 1 -i brightness | awk '{print $2}')
--- startup.sh
+++ startup.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
 sleep 3
-xrandr --output eDP-1 --off
-xrandr --output DP-1 --auto --mode 1920x1080 --brightness 0.7
-nmcli device disconnect enxacde48001122 # disabling ethernet on startup
+
+# Check if external monitor is connected
+if xrandr | grep "^HDMI-1 connected"; then
+    # External monitor is connected
+    xrandr --output eDP-1 --off
+	# xrandr --output DP-1 --auto --mode 1920x1080 --brightness 0.7
+    xrandr --output HDMI-1 --auto --mode 1920x1080 --brightness 0.7
+else
+    # External monitor is not connected, enable laptop screen
+    xrandr --output eDP-1 --auto
+fi
+
+
+# nmcli device disconnect enxacde48001122 # disabling ethernet on startup
 setxkbmap -option caps:swapescape
 i3-msg restart
 
