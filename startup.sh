13cb644a8f5107e1e2b26881231dc26ff7d02ff6
Sriram Suresh <sriram.suresh449@gmail.com>
2025-07-30 19:27:59 -0700

    made changes to get copyq to work with tray enabled

--- startup.sh
+++ startup.sh
@@ -20,7 +20,7 @@ setxkbmap -option caps:swapescape
 i3-msg restart
 
 # bluetoothctl connect 08:EB:ED:4C:ED:1F # insignia mini sonic
-# bluetoothctl connect 08:EB:ED:D2:A8:E5 # insignia mini sonic (new MAC address?!)
+bluetoothctl connect 08:EB:ED:D2:A8:E5 # insignia mini sonic (new MAC address?!)
 # bluetoothctl connect 4C:FE:2E:28:70:72 # jvc ha-a3t
 bluetoothctl connect E8:9E:13:09:D4:4D # wh-ch520
 
