7fa73904d14bba5396a046229360f85fad1a26d3
Sriram Suresh <sriram.suresh449@gmail.com>
2025-07-30 20:57:18 -0700

    fixed display switching scripts

--- arandr_config.sh
+++ arandr_config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # NOTE: this command sets the external monitor to be the primary, as set by the flag
-xrandr --output eDP-1 --mode 1366x768 --pos 0x156 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --rate 75 --pos 2718x0 --rotate normal
+xrandr --output eDP --mode 1440x900 --pos 0x0 --rotate normal --output DisplayPort-3 --primary --mode 1920x1080 --rate 75 --pos 1568x0 --rotate normal
+
