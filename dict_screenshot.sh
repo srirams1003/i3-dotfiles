ee21d198567e5f84445271737b95f204926d53ab
Sriram Suresh <sriram.suresh449@gmail.com>
2025-09-01 20:44:55 -0700

    modified my favorite keybinds to use google search in a new firefox window instead of using goldendict

--- dict_screenshot.sh
+++ dict_screenshot.sh
@@ -1,11 +1,27 @@
+# # #!/bin/bash
+# # maim --format=png --select "/tmp/myverytemporarypicture.png" && 
+# # tesseract /tmp/myverytemporarypicture.png /tmp/myverytemporaryoutput && 
+# # word=$(cat /tmp/myverytemporaryoutput.txt) &&
+# # dict "$word" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+#
 # #!/bin/bash
 # maim --format=png --select "/tmp/myverytemporarypicture.png" && 
 # tesseract /tmp/myverytemporarypicture.png /tmp/myverytemporaryoutput && 
 # word=$(cat /tmp/myverytemporaryoutput.txt) &&
-# dict "$word" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+# goldendict "$word"
+#
 
 #!/bin/bash
-maim --format=png --select "/tmp/myverytemporarypicture.png" && 
-tesseract /tmp/myverytemporarypicture.png /tmp/myverytemporaryoutput && 
-word=$(cat /tmp/myverytemporaryoutput.txt) &&
-goldendict "$word"
+
+# Take a screenshot of a selected area
+maim --format=png --select "/tmp/myverytemporarypicture.png" && \
+
+# Run OCR on the screenshot
+tesseract /tmp/myverytemporarypicture.png /tmp/myverytemporaryoutput && \
+
+# Read the recognized text
+word=$(cat /tmp/myverytemporaryoutput.txt) && \
+
+# Open a new Firefox window with Google search
+firefox --new-window "https://www.google.com/search?q=$(echo "$word" | sed 's/ /+/g')" &
+
--- dict_selection.sh
+++ dict_selection.sh
@@ -1,7 +1,16 @@
+# # #!/bin/bash
+# # selected_text=$(xclip -o -selection primary)
+# # dict "$selected_text" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+#
 # #!/bin/bash
 # selected_text=$(xclip -o -selection primary)
-# dict "$selected_text" | zenity --text-info --title="Dictionary Definition" --width=1920 --height=1080
+# goldendict "$selected_text"
 
 #!/bin/bash
+
+# Get the highlighted text (from PRIMARY selection)
 selected_text=$(xclip -o -selection primary)
-goldendict "$selected_text"
+
+# Launch Firefox with the search query
+firefox --new-window "https://www.google.com/search?q=$(echo "$selected_text" | sed 's/ /+/g')" &
+
