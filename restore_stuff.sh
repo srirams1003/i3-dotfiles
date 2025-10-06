ce7f7ddba49f6315931675de33255c28bea3413e
Sriram Suresh <sriram.suresh449@gmail.com>
2025-10-05 17:16:07 -0700

    removed firefox stuff

--- restore_stuff.sh
+++ restore_stuff.sh
@@ -12,8 +12,8 @@ ln -sf "$latest_file" last
 # Restore zsh history
 cp /home/sriram/.config/i3/my_zsh_history /home/sriram/.zsh_history
 
-# Restore Firefox profile
-pkill firefox
-rm -rf ~/.mozilla/firefox
-cp -r /home/sriram/.config/i3/firefox-backup/* ~/.mozilla/firefox/
-chown -R $USER:$USER ~/.mozilla/firefox
+# # Restore Firefox profile
+# pkill firefox
+# rm -rf ~/.mozilla/firefox
+# cp -r /home/sriram/.config/i3/firefox-backup/* ~/.mozilla/firefox/
+# chown -R $USER:$USER ~/.mozilla/firefox
